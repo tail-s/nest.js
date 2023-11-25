5adbdb0c00051b4984226203f4133f0f02a3ede4
tail-s <lunar7s@naver.com>
2023-11-26 06:15:10 +0900

    23.11.26

--- nestjs-board-app/src/auth/auth.module.ts
+++ nestjs-board-app/src/auth/auth.module.ts
@@ -5,14 +5,17 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'mynestjsboardpjt',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 60 * 60,
+        expiresIn: jwtConfig.expiresIn,
       },
     }),
   ],
