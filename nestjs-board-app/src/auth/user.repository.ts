f22e8ef48010eb162858bb0d518f238dbcae9c4c
tail-s <lunar7s@naver.com>
2023-11-26 02:04:01 +0900

    23.11.26

---
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -0,0 +1,10 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+}
